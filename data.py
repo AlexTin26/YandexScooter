b66a77375bcda1958145b7b31a939a8c185655d3
Alexandr <kovtun_2003@bk.ru>
2023-09-11 21:20:33 +0300

    Yandex_Scooter

---
+++ data.py
@@ -0,0 +1,15 @@
+class TestData:
+    BASE_URL = 'https://b47814ab-039c-4be5-9501-15ce18628abd.serverhub.praktikum-services.ru'
+    ORDER_PAYLOAD = {
+        "firstName": "Naruto",
+        "lastName": "Uchiha",
+        "address": "Konoha, 142 apt.",
+        "metroStation": 4,
+        "phone": "+7 800 355 35 35",
+        "rentTime": 5,
+        "deliveryDate": "2020-06-06",
+        "comment": "Saske, come back to Konoha",
+        "color": [
+            "BLACK"
+        ]
+    }
